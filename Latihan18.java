734aa43696c7d7b4e88a05685012af7e5529f6cf
Muhammad Fauzan <muhammadfauzanfadhlulbarr@gmail.com>
2020-10-30 21:14:35 +0700

    first commit

---
+++ Latihan18.java
@@ -0,0 +1,17 @@
+/**
+ * Latihan17
+ * Nama     : Muhammad Fauzan Fadhlulbarr
+ * Kelas    : PBO10K
+ * NIM      : 10119176
+ * Program : Kelipatan
+ */
+public class Latihan18 {
+   public static void main(String[] args) {
+       double bil;
+       for(bil=3.5;bil<=35;bil+=3.5){
+        if(bil%3.5==0){
+            System.out.println(bil);
+        }
+       }
+   } 
+}
